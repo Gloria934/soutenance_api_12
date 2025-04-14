ef3a051bf5487980e59904be17a406802ab5e9a8
Gloria Kht <khtgloria@gmail.com>
2025-04-14 10:58:02 +0100

    création des migrations (pas encore terminé)

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,11 +13,15 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nom');
+            $table->string('prenom');
+            $table->string('telephone');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->longText('profile_illustratif');
             $table->rememberToken();
+            $table->softDeletes();
             $table->timestamps();
         });
 
---
+++ database/migrations/2025_04_14_092800_create_services_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('services', function (Blueprint $table) {
+            $table->id();
+            $table->string('nom');
+            $table->float('prix_rdv');
+            $table->time('heure_ouverture');
+            $table->time('heure_fermeture');
+            $table->time('duree_moy_rdv');
+            $table->boolean('sous_rdv');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('services');
+    }
+};
