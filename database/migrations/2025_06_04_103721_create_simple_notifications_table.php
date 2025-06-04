ee7a546c19984068250e37156326980451554b33
@obedsallai <adifonobedsallai@gmail.com>
2025-06-04 18:53:36 +0200

    correction des erreurs de données pour l'affichage des notifications pour l'administrateur

--- database/migrations/2025_06_04_103721_create_simple_notifications_table.php
+++ database/migrations/2025_06_04_103721_create_simple_notifications_table.php
@@ -4,11 +4,11 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateNotificationsTable extends Migration
+class CreateSimpleNotificationsTable extends Migration
 {
     public function up()
     {
-        Schema::create('notifications', function (Blueprint $table) {
+        Schema::create('simple_notifications', function (Blueprint $table) {
             $table->id();
             $table->foreignId('personnel_sante_id')->constrained('users')->onDelete('cascade');
             $table->string('type')->default('personnel_registration'); // Type de notification
@@ -19,6 +19,6 @@ class CreateNotificationsTable extends Migration
 
     public function down()
     {
-        Schema::dropIfExists('notifications');
+        Schema::dropIfExists('simple_notifications');
     }
 }
