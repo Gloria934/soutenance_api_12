6e094de5544c76a9a74aa97bd50c8169bae9eb1c
Gloria Kht <khtgloria@gmail.com>
2025-06-18 15:09:33 +0100

    ajout des modifications dans la fonction d'authentification pour gérer les réponses de connexions

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\ValidationException;
 use Illuminate\Support\Facades\Password;
+use Illuminate\Support\Facades\Hash;
 
 class AuthenticatedSessionController extends Controller
 {
