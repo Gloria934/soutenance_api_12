bc08807a05513c8fb975c18d043855e584c3ad7e
obed sallai <adifonobedsallai@gmail.com>
2025-09-24 10:26:37 +0200

    correction de seeders dupiqué

--- database/seeders/ServiceSeeder.php
+++ database/seeders/ServiceSeeder.php
@@ -100,7 +100,7 @@ class ServiceSeeder extends Seeder
         ];
 
         foreach ($services as $service) {
-            Service::create($service);
+            Service::firstOrCreate(['email' => $service['email']], $service);
         }
     }
 }
