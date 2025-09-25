eae28d3b1fb122cbae98ef5e270465db81e1644d
obed sallai <adifonobedsallai@gmail.com>
2025-09-25 21:58:24 +0200

    donner les permissions à Nginx

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo "Configuration des permissions..."
-chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
+chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache /var/lib/nginx
 chmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache
 
 echo "Exécution des migrations..."
